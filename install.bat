69443c97884bee2b45b1db30890b7ad1058baab9
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-01-07 08:41:03 +0530

    save

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
-rem   set JAVA_HOME=D:\dev\pub\java\jdk-18.0.2.1
-rem set M3_HOME=D:\dev\pub\maven\apache-maven-3.8.4
-rem set PATH=%JAVA_HOM%\bin;%M3_HOME%\bin
+set JAVA_HOME=D:\dev\pub\java\jdk-18.0.2.1
+set M3_HOME=D:\dev\pub\maven\apache-maven-3.8.4
+set PATH=%JAVA_HOME%\bin;%M3_HOME%\bin
 rem mvn clean install -Drevision=1.0.0-SNAPSHOT
+
 call java -cp maven.rev.set.jar maven.rev.set.PomReviser . mvn.cmd clean install
